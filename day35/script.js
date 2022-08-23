23a21cf7c0607aa488479cb29ce74b2335315012
bxavace <b_xavace@outlook.com>
2022-08-23 18:41:27 +0800

    auto carousel slider with btns

---
+++ day35/script.js
@@ -0,0 +1,44 @@
+const imgs = document.getElementById("imgs");
+const leftBtn = document.getElementById("left");
+const rightBtn = document.getElementById("right");
+
+const img = document.querySelectorAll("#imgs img");
+
+let i = 0;
+
+let interval = setInterval(run, 2000);
+
+function run() {
+  i++;
+
+  changeImage();
+}
+
+function changeImage() {
+  if (i > img.length - 1) {
+    i = 0;
+  } else if (i < 0) {
+    i = img.length - 1;
+  }
+
+  imgs.style.transform = `translateX(${-i * 479}px)`;
+}
+
+function resetInterval() {
+  clearInterval(interval);
+  interval = setInterval(run, 2000);
+}
+
+rightBtn.addEventListener("click", () => {
+  i++;
+
+  changeImage();
+  resetInterval();
+});
+
+leftBtn.addEventListener("click", () => {
+  i--;
+
+  changeImage();
+  resetInterval();
+});
