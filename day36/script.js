1b13ace5194306224c997e7389af5a3eb76096a5
bxavace <b_xavace@outlook.com>
2022-08-24 20:22:13 +0800

    led hoverboard

---
+++ day36/script.js
@@ -0,0 +1,36 @@
+const container = document.getElementById("container");
+const colors = [
+  "#ec422f",
+  "#e9662a",
+  "#e9d02a",
+  "#28e628",
+  "#313ae8",
+  "#6531e8",
+];
+
+const SQUARES = 500;
+
+for (let i = 0; i < SQUARES; i++) {
+  const square = document.createElement("div");
+  square.classList.add("square");
+
+  square.addEventListener("mouseover", () => setColor(square));
+  square.addEventListener("mouseout", () => removeColor(square));
+
+  container.appendChild(square);
+}
+
+function setColor(element) {
+  const color = getRandomColor();
+  element.style.background = color;
+  element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`;
+}
+
+function removeColor(element) {
+  element.style.background = "#1d1d1d";
+  element.style.boxShadow = "0 0 2px #000";
+}
+
+function getRandomColor() {
+  return colors[Math.floor(Math.random() * colors.length)];
+}
